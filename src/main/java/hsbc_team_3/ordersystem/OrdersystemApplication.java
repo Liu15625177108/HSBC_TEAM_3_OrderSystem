c395970eda663bfa6b45025806b6cd4fc4344da9
Liu15625177108 <2447152234@qq.com>
2018-08-01 16:20:53 +0800

    init

---
+++ src/main/java/hsbc_team_3/ordersystem/OrdersystemApplication.java
@@ -0,0 +1,12 @@
+package hsbc_team_3.ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersystemApplication.class, args);
+    }
+}
---
+++ src/test/java/hsbc_team_3/ordersystem/OrdersystemApplicationTests.java
@@ -0,0 +1,16 @@
+package hsbc_team_3.ordersystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdersystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
