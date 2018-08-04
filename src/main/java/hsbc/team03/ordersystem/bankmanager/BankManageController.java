c437c126548f97618f792c01e318faaae9f54acd
Liu15625177108 <2447152234@qq.com>
2018-08-04 09:50:20 +0800

    modify the package

--- src/main/java/hsbc/team03/ordersystem/bankmanager/BankManageController.java
+++ src/main/java/hsbc/team03/ordersystem/bankmanager/BankManageController.java
@@ -1,11 +1,10 @@
-package hsbc_team_3.ordersystem.bankmanager;
+package hsbc.team03.ordersystem.bankmanager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.security.util.Password;
 
 /**
  * @ClassName BankManageController
