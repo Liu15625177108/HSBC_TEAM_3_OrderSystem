1ee5c53e0003c9d8967542bf55619cf5fdec4d8b
CHENL0 <893457378@qq.com>
2018-08-08 17:37:31 +0800

    add interceptor

--- src/main/java/hsbc.team03/ordersystem/product/OrderController.java
+++ src/main/java/hsbc.team03/ordersystem/product/OrderController.java
@@ -21,67 +21,56 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * @description〈the controller of order〉
  * @author Chen
+ * @description〈the controller of order〉
  * @create 2018/8/2
  * @since 1.0.0
  */
 @Slf4j
 @Controller
-@RequestMapping(value = "/order" , produces = "application/json; charset=utf-8")
+@RequestMapping(value = "/order", produces = "application/json; charset=utf-8")
 public class OrderController {
     @Autowired
     private OrderService orderService;
     @Autowired
     private UserService userService;
-    
+
     /**
+     * @return java.lang.Object
      * @Author Chen
      * @Description //TODO  To place an order
      * @Date 5:52 2018/8/3
      * @Param []
-     * @return java.lang.Object
      **/
     @PostMapping(value = "/toorder")
     public @ResponseBody
-    Object toOrder( @RequestBody ProductInfo productInfo,String payPassword,HttpServletRequest request){
+    Object toOrder(@RequestBody ProductInfo productInfo, String payPassword, HttpServletRequest request) {
         UserInfo userInfo = (UserInfo) request.getSession().getAttribute("userInfo");
-        System.out.println(userInfo.getUserName());
-        System.out.println(productInfo.getProdcutNumber());
-        if(userInfo!=null&&!userInfo.equals("")) {
-            
-            //To compare userMoney and orderPrice
-            if (userInfo.getUserMoney()> orderService.getOrderPrice(productInfo.getProdcutNumber(),productInfo.getProdcutPrice())) {
-                
-                //to check userPayPassword
-                if (userService.toValidatePayPassword(userInfo,payPassword)) {
-                    
-                    Object resultView = orderService.toOrder(productInfo,userInfo);
-                    if(resultView==null){
-                    System.out.println("+++---");}
-                    String result = resultView.toString();
-                    log.info(result);
-                    return resultView;
-                    
-                }
-                
-                String result = "Sorry,your password is wrong,please reenter it";
+
+        //To compare userMoney and orderPrice
+        if (userInfo.getUserMoney() > orderService.getOrderPrice(productInfo.getProdcutNumber(), productInfo.getProdcutPrice())) {
+
+            //to check userPayPassword
+            if (userService.toValidatePayPassword(userInfo, payPassword)) {
+
+                Object resultView = orderService.toOrder(productInfo, userInfo);
+                String result = resultView.toString();
                 log.info(result);
-                ResultView resultView = new ResultView<String>(401, "error", result);
                 return resultView;
-                
+                 
             }
-            
-            String result = "Sorry,your money is not enough,please recharge";
+
+            String result = "Sorry,your password is wrong,please reenter it";
             log.info(result);
             ResultView resultView = new ResultView<String>(401, "error", result);
             return resultView;
+
         }
-        
-        String result = "Sorry,you haven't login,please login";
+
+        String result = "Sorry,your money is not enough,please recharge";
         log.info(result);
         ResultView resultView = new ResultView<String>(401, "error", result);
         return resultView;
-        
     }
+
 }
