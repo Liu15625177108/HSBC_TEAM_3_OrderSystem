c4ccd024e4b81bb879c6aa869499bbde3ca85e16
EvanYaoQiHui <13612246284@163.com>
2018-08-09 11:41:09 +0800

    L

--- src/main/java/hsbc/team03/ordersystem/displayproduct/ProductController.java
+++ src/main/java/hsbc/team03/ordersystem/displayproduct/ProductController.java
@@ -1,15 +1,13 @@
 package hsbc.team03.ordersystem.displayproduct;
 
-import hsbc.team03.ordersystem.displayproduct.common.SortUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @Author:Evan
@@ -22,26 +20,27 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductService productService;
+
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
     /**
-    * @Description:  get products and sort by product's type
-    * @Author:        @Evan
-    * @CreateDate:   2018/8/5 22:14
-    * @UpdateDate:   2018/8/5 22:14
-    * @Version:      1.0
-    */
+     * @Description: get products and sort by product's type
+     * @Author: @Evan
+     * @CreateDate: 2018/8/5 22:14
+     * @UpdateDate: 2018/8/5 22:14
+     * @Version: 1.0
+     */
     @RequestMapping(value = "/user/get/products", method = RequestMethod.GET)
-    public Page<Product> getProduct(int pageNum) {
-      /*  List<Product> list = this.productService.getAllProduct();
-        *//*sort by product's type*//*
+    public Page<Product> getProduct(@RequestParam(value = "pageNum", defaultValue = "0") int pageNum) {
+        /*  List<Product> list = this.productService.getAllProduct();
+         *//*sort by product's type*//*
         SortUtils.sort(list, "type", null);
         */
-        Sort sort=Sort.by("type");
-        PageRequest request =PageRequest.of(pageNum,2,sort);
-
-       Page<Product> products= productService.getProductListByPage(request);
+        Sort sort = Sort.by("type");
+        PageRequest request = PageRequest.of(pageNum, 2, sort);
+        Page<Product> products = productService.getProductListByPage(request);
         return products;
     }
 }
