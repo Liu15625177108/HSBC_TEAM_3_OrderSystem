55cc6d30cc3b8004198696c5e103fe7b26a6d92e
EvanYaoQiHui <13612246284@163.com>
2018-08-13 22:38:24 +0800

    finish Angular

--- src/main/java/hsbc/team03/ordersystem/displayproduct/ProductController.java
+++ src/main/java/hsbc/team03/ordersystem/displayproduct/ProductController.java
@@ -1,13 +1,14 @@
 package hsbc.team03.ordersystem.displayproduct;
 
+import hsbc.team03.ordersystem.displayproduct.common.SortUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * @Author:Evan
@@ -32,15 +33,23 @@ public class ProductController {
      * @UpdateDate: 2018/8/5 22:14
      * @Version: 1.0
      */
+
+    @GetMapping("/productIndex")
+    public ModelAndView productIndex(){
+        ModelAndView mv=new ModelAndView("displayProduct.html");
+        return mv;
+    }
+
     @RequestMapping(value = "/user/get/products", method = RequestMethod.GET)
-    public Page<Product> getProduct(@RequestParam(value = "pageNum", defaultValue = "0") int pageNum) {
-        /*  List<Product> list = this.productService.getAllProduct();
-         *//*sort by product's type*//*
+    public List<Product> getProduct() {
+       List<Product> list = this.productService.getAllProduct();
+         //*sort by product's type*//*
         SortUtils.sort(list, "type", null);
-        */
         Sort sort = Sort.by("type");
-        PageRequest request = PageRequest.of(pageNum, 2, sort);
-        Page<Product> products = productService.getProductListByPage(request);
-        return products;
+
+//        PageRequest request = PageRequest.of(pageNum, 2, sort);
+       // @RequestParam(value = "pageNum", defaultValue = "0") int pageNum
+//        Page<Product> products = productService.getProductListByPage(request);
+        return list;
     }
 }
